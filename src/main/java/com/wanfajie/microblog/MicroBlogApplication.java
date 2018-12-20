7a69adfaeba28c558e5adbae08389c6367c81095
WaferJay <474346873@qq.com>
2018-12-20 23:09:26 +0800

    配置打包war

--- src/main/java/com/wanfajie/microblog/MicroBlogApplication.java
+++ src/main/java/com/wanfajie/microblog/MicroBlogApplication.java
@@ -2,13 +2,21 @@ package com.wanfajie.microblog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.support.ConfigurableWebBindingInitializer;
 
 @SpringBootApplication
 @ComponentScan
 @ServletComponentScan
-public class MicroBlogApplication {
+public class MicroBlogApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+	    return builder.sources(MicroBlogApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MicroBlogApplication.class, args);
