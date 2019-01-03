8b1cf6ecf052cec8c526eaeef491ba6358c67aff
WaferJay <474346873@qq.com>
2019-01-03 17:01:55 +0800

    fix: 添加用于分页的count语句

--- src/main/java/com/wanfajie/microblog/repository/UserRepository.java
+++ src/main/java/com/wanfajie/microblog/repository/UserRepository.java
@@ -21,12 +21,18 @@ public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificat
 
     @Query(value = "select u.* from mb_user_subscribe as us " +
             "inner join mb_user as u on u.id = us.following_id " +
-            "where us.follower_id = :userId", nativeQuery = true)
+            "where us.follower_id = :userId",
+            countQuery = "SELECT count(us.following_id) FROM mb_user_subscribe AS us \n" +
+            "INNER JOIN mb_user AS u ON u.id = us.following_id \n" +
+            "WHERE us.follower_id = :userId", nativeQuery = true)
     Page<User> findFollowings(@Param("userId") long userId, Pageable pageable);
 
     @Query(value = "select u.* from mb_user_subscribe as us " +
             "inner join mb_user as u on u.id = us.follower_id " +
-            "where us.following_id = :userId", nativeQuery = true)
+            "where us.following_id = :userId",
+            countQuery = "SELECT count(us.follower_id) FROM mb_user_subscribe AS us \n" +
+            "INNER JOIN mb_user AS u ON u.id = us.follower_id \n" +
+            "WHERE us.following_id = :userId", nativeQuery = true)
     Page<User> findFollowers(@Param("userId") long userId, Pageable pageable);
 
     // XXX: int转boolean
